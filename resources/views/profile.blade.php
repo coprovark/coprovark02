12ab15938211b5453b08af5eb6e87a3112b758de
chatpituk <chatpituk.ed57@ubru.ac.th>
2018-03-07 15:39:00 +0700

    #profile#web

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,8 @@
+Name-Chatpituk Phateechoei <br>
+Nick name-Oat <br>
+Age-22
+Sex-Male <br>
+Hobby-play game <br>
+Like-Chocolate <br>
+Dislike-Durian <br>
+Aim-Create AAA game or animation 
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return view('hello');
 });
+Route::get('/profile', function () {
+    return view('profile');
+});
